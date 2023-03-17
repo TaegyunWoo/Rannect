0a1d24ed231cb897ca08be4e589b5bca0844751e
TaegyunWoo <dnxprbs@gmail.com>
2023-03-17 12:55:17 +0900

    [BE] Init Server Application

---
+++ rannect-back/general/src/main/java/kr/pe/rannect/general/RannectGeneralApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.rannect.general;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RannectGeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RannectGeneralApplication.class, args);
+	}
+
+}
---
+++ rannect-back/general/src/test/java/kr/pe/rannect/general/RannectGeneralApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.rannect.general;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RannectGeneralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
