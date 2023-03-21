6801144306cb0219532aaae6a41e6be533c4974b
TaegyunWoo <dnxprbs@gmail.com>
2023-03-21 16:42:03 +0900

    [BE] Modified Main Class Name

--- rannect-back/general/src/main/java/kr/pe/rannect/randomchat/RannectRandomChatApplication.java
+++ rannect-back/general/src/main/java/kr/pe/rannect/randomchat/RannectRandomChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RannectGeneralApplication {
+public class RannectRandomChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RannectGeneralApplication.class, args);
+		SpringApplication.run(RannectRandomChatApplication.class, args);
 	}
 
 }
