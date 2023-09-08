012f13492b6d74e7a389bbcb69ce9255a4833d37
TaegyunWoo <dnxprbs@gmail.com>
2023-09-08 23:50:37 +0900

    [BE] Add local DB Naming Strategy

---
+++ rannect-back/general/src/main/java/kr/pe/rannect/randomchat/utils/jpa/UpperCaseNamingStrategy.java
@@ -0,0 +1,19 @@
+/**
+ * author         : 우태균
+ * description    : JPA Configuration이 DDL AUTO인 경우, DB 테이블 및 칼럼 네이밍 전략.
+ *                  모두 대문자 + 언더바 조합으로 생성함.
+ */
+package kr.pe.rannect.randomchat.utils.jpa;
+
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+import java.util.Locale;
+
+public class UpperCaseNamingStrategy extends CamelCaseToUnderscoresNamingStrategy {
+  @Override
+  protected Identifier getIdentifier(String name, boolean quoted, JdbcEnvironment jdbcEnvironment) {
+    return new Identifier(name.toUpperCase(Locale.ROOT), quoted);
+  }
+}
