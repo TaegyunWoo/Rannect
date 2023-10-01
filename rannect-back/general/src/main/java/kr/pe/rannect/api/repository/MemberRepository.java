aeb2e6e2eba6aae0b5c2aaf6901ea84cc14980de
TaegyunWoo <dnxprbs@gmail.com>
2023-10-01 21:53:10 +0900

    [BE] Migrate to Spring Data JPA

--- rannect-back/general/src/main/java/kr/pe/rannect/api/repository/MemberRepository.java
+++ rannect-back/general/src/main/java/kr/pe/rannect/api/repository/MemberRepository.java
@@ -5,43 +5,12 @@
 package kr.pe.rannect.api.repository;
 
 import kr.pe.rannect.api.domain.Member;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.util.Optional;
 
 @Repository
-public class MemberRepository {
-  @PersistenceContext
-  private EntityManager entityManager;
-
-  /**
-   * 사용자 엔티티 저장
-   * @param member 저장할 엔티티
-   */
-  public void save(Member member) {
-    entityManager.persist(member);
-  }
-
-  /**
-   * 계정 ID로 사용자 조회
-   * @param accountId 조회할 계정 ID
-   * @return 조회 결과 (결과가 없다면, Optional.empty())
-   */
-  public Optional<Member> findByAccountId(String accountId) {
-    String jpql = "select m from Member m" +
-        " where m.accountId = :accountId";
-    TypedQuery<Member> query = entityManager.createQuery(jpql, Member.class)
-        .setParameter("accountId", accountId);
-
-    try {
-      Member result = query.getSingleResult();
-      return Optional.of(result);
-    } catch (NoResultException e) {
-      return Optional.empty();
-    }
-  }
+public interface MemberRepository extends CrudRepository<Member, Long> {
+  Optional<Member> findByAccountId(String accountId);
 }
