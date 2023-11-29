ec534eceb62bd26af0c21f20b3f9498b96d59107
TaegyunWoo <dnxprbs@gmail.com>
2023-11-29 18:25:34 +0900

    [BE] Add JwtTokenParser 유틸리티 컴포넌트

---
+++ rannect-back/general/src/main/java/kr/pe/rannect/api/utils/token/JwtTokenParser.java
@@ -0,0 +1,60 @@
+/**
+ * author         : 우태균
+ * description    : 토큰 파싱 유틸리티 컴포넌트
+ */
+package kr.pe.rannect.api.utils.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import kr.pe.rannect.api.utils.date.LocalDateTimeTransformer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+public class JwtTokenParser {
+  private final String secretKey;
+  private final LocalDateTimeTransformer localDateTimeTransformer;
+
+  /**
+   * 생성자를 통해, auth-secret.properties 에 정의된 값을 final 필드에 바인딩
+   * @param secretKey 비밀키
+   * @param localDateTimeTransformer Date->LocalDate 변환 컴포넌트
+   */
+  public JwtTokenParser(@Value("${jwt.secret}") String secretKey, LocalDateTimeTransformer localDateTimeTransformer) {
+    this.secretKey = secretKey;
+    this.localDateTimeTransformer = localDateTimeTransformer;
+  }
+
+  /**
+   * 유효한 JWT 토큰 클레임을 파싱하는 메서드
+   * 반환된 Optional이 null이라면, 유효하지 않은 토큰
+   * @param tokenValue 토큰값
+   * @return 토큰이 정상적인 형태라면, 토큰의 클레임들을 Optional로 Wrapping하여 반환
+   */
+  public Optional<Claims> parseJwtToken(String tokenValue) throws JwtException {
+    return Optional.ofNullable(
+        Jwts.parser()
+            .setSigningKey(secretKey)
+            .parseClaimsJws(tokenValue)
+            .getBody());
+  }
+
+  /**
+   * 토큰 만료기간 추출 메서드
+   * @param tokenValue 토큰 값
+   * @return 만료기간을 LocalDateTime 으로 반환
+   */
+  public LocalDateTime extractExpiration(String tokenValue) {
+    Date expiration = Jwts.parser()
+        .setSigningKey(secretKey)
+        .parseClaimsJws(tokenValue)
+        .getBody()
+        .getExpiration();
+    return localDateTimeTransformer.dateToLocalDateTime(expiration);
+  }
+}
---
+++ rannect-back/general/src/test/java/kr/pe/rannect/api/utils/token/JwtTokenParserTest.java
@@ -0,0 +1,143 @@
+package kr.pe.rannect.api.utils.token;
+
+import com.google.common.base.Supplier;
+import io.jsonwebtoken.*;
+import kr.pe.rannect.api.utils.date.LocalDateTimeTransformer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Executable;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+class JwtTokenParserTest {
+  private final String secretKey = "my secret key";
+  @Mock
+  private LocalDateTimeTransformer localDateTimeTransformer;
+  private JwtTokenParser jwtTokenParser;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    jwtTokenParser = new JwtTokenParser(secretKey, localDateTimeTransformer);
+  }
+
+  @Test
+  void 유효한_JWT_토큰에서_클레임을_파싱() {
+    //GIVEN
+    Date now = new Date();
+    String claimName = "myClaimName";
+    String claimVal = "myClaimVal";
+    String validTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(new Date(now.getTime() + 2000000000))
+        .claim(claimName, claimVal)
+        .signWith(SignatureAlgorithm.HS512, secretKey)
+        .compact();
+
+    //WHEN
+    Optional<Claims> claims = jwtTokenParser.parseJwtToken(validTokenVal);
+
+    //THEN
+    assertTrue(claims.isPresent());
+    assertEquals(claimVal, claims.get().get(claimName, String.class));
+  }
+
+  @Test
+  void 만료된_JWT_토큰에서_클레임을_파싱() {
+    //GIVEN
+    Date now = new Date();
+    String invalidTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(new Date(now.getTime() + 2000000000))
+        .signWith(SignatureAlgorithm.HS512, secretKey)
+        .compact();
+
+    //WHEN-THEN
+    assertThrows(ExpiredJwtException.class, () -> jwtTokenParser.parseJwtToken(invalidTokenVal));
+  }
+
+  @Test
+  void 다른_대칭키로_해싱된_JWT_토큰에서_클레임을_파싱() {
+    //GIVEN
+    Date now = new Date();
+    String invalidTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(new Date(now.getTime() - 2000000))
+        .signWith(SignatureAlgorithm.HS512, "different secret key val")
+        .compact();
+
+    //WHEN-THEN
+    assertThrows(SignatureException.class, () -> jwtTokenParser.parseJwtToken(invalidTokenVal));
+  }
+
+  @Test
+  void 유효한_JWT_토큰에서_만료기간_추출() {
+    //GIVEN
+    Date now = new Date();
+    Date expireAtDate = new Date(now.getTime() + 2000000000);
+    LocalDateTime expireAtLocalDateTime = LocalDateTime.MAX;
+    String validTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(expireAtDate)
+        .signWith(SignatureAlgorithm.HS512, secretKey)
+        .compact();
+
+    //(mock)
+    given(localDateTimeTransformer.dateToLocalDateTime(any())).willReturn(expireAtLocalDateTime);
+
+    //WHEN
+    LocalDateTime result = jwtTokenParser.extractExpiration(validTokenVal);
+
+    //THEN
+    then(localDateTimeTransformer).should(times(1)).dateToLocalDateTime(any());
+    assertEquals(expireAtLocalDateTime, result);
+  }
+
+  @Test
+  void 만료된_JWT_토큰에서_만료기간_추출() {
+    //GIVEN
+    Date now = new Date();
+    Date expireAtDate = new Date(now.getTime() - 2000000000);
+    String expiredTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(expireAtDate)
+        .signWith(SignatureAlgorithm.HS512, secretKey)
+        .compact();
+
+    //WHEN-THEN
+    assertThrows(ExpiredJwtException.class, () -> jwtTokenParser.extractExpiration(expiredTokenVal));
+  }
+
+  @Test
+  void 다른_대칭키로_해싱된_토큰에서_만료기간_추출() {
+    //GIVEN
+    Date now = new Date();
+    Date expireAtDate = new Date(now.getTime() + 2000000000);
+    String invalidTokenVal = Jwts.builder()
+        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+        .setIssuedAt(now)
+        .setExpiration(expireAtDate)
+        .signWith(SignatureAlgorithm.HS512, "different secret key val")
+        .compact();
+
+    //WHEN-THEN
+    assertThrows(SignatureException.class, () -> jwtTokenParser.extractExpiration(invalidTokenVal));
+  }
+}
