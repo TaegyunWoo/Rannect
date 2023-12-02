a4ec1635e98cc83f63bfd4dede4fc887abba0df9
TaegyunWoo <dnxprbs@gmail.com>
2023-12-02 12:51:53 +0900

    [BE] 테스트용 계정 추가

---
+++ rannect-back/general/src/main/resources/data-h2.sql
@@ -0,0 +1,5 @@
+-- ====================== Description ======================
+-- 로컬 환경에서 실행시킬 DML 쿼리 (for h2 DB)
+-- ====================================================
+
+INSERT INTO `MEMBER`(`ID`, `ACCOUNT_ID`, `PASSWORD`, `NICKNAME`, `INTERESTED_IN`, `CREATED_DATETIME`, `MODIFIED_DATETIME`) VALUES(1, 'test01','$2a$10$JgyoEgQFBTpxIIkhcgdN1OxsMpNlKfwzE2ljUcv9RutzU5c/kg4WW', 'test01', 'test01_interested_in', '2023-12-02 12:47:07.52016', '2023-12-02 12:47:07.52016');
