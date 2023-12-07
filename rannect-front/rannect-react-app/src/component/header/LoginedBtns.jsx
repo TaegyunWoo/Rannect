143d97c530d852c389bad6e4e8052871553f9ea8
TaegyunWoo <dnxprbs@gmail.com>
2023-12-07 14:37:28 +0900

    [FE] UI 오류 수정

--- rannect-front/rannect-react-app/src/component/header/LoginedBtns.jsx
+++ rannect-front/rannect-react-app/src/component/header/LoginedBtns.jsx
@@ -3,14 +3,15 @@ import React from "react";
 function LoginedBtns({ handleClick, nickname }) {
   return (
     <div
-      className="col-sm-4 float-end mt-auto user-setting-btn"
+      className="col-sm-4 mt-auto user-setting-btn"
+      style={{ float: "right" }}
       onClick={handleClick}
     >
-      <span className="col-sm-9 text-end float-end">
+      <span className="col-sm-9 text-end" style={{ float: "right" }}>
         로그인한 사용자 :{" "}
         <span className="badge">{nickname ? nickname : ""}</span>
+        <img src="/icon/person-circle.svg" className="col-sm-3 ms-2" />
       </span>
-      <img src="/icon/person-circle.svg" className="col-sm-2 ms-2 float-end" />
     </div>
   );
 }
--- rannect-front/rannect-react-app/src/component/header/NotLoginedBtns.jsx
+++ rannect-front/rannect-react-app/src/component/header/NotLoginedBtns.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 function NotLoginedBtns({ handleSignUpClick, handleSignInClick }) {
   return (
-    <div className={`col-sm-4 float-end mt-auto user-setting-btn`}>
+    <div
+      className={`col-sm-4 mt-auto user-setting-btn`}
+      style={{ float: "right" }}
+    >
       <button
         type="button"
-        className="btn float-end mx-2"
+        className="btn mx-2"
         onClick={handleSignUpClick}
-        style={{ color: "whitesmoke", backgroundColor: "#8C52FF" }}
+        style={{
+          color: "whitesmoke",
+          backgroundColor: "#8C52FF",
+          float: "right",
+        }}
       >
         회원가입
       </button>
@@ -15,7 +22,11 @@ function NotLoginedBtns({ handleSignUpClick, handleSignInClick }) {
         type="button"
         className="btn float-end mx-2"
         onClick={handleSignInClick}
-        style={{ color: "whitesmoke", backgroundColor: "#8C52FF" }}
+        style={{
+          color: "whitesmoke",
+          backgroundColor: "#8C52FF",
+          float: "right",
+        }}
       >
         로그인
       </button>
